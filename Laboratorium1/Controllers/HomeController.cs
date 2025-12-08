6c287753035702209aca1a618027204d7220ecae
Bartek-Niedzielski <bartlomiej.niedzielski@onet.pl>
2025-12-08 15:33:53 +0100

    Calc+widok

--- Laboratorium1/Controllers/HomeController.cs
+++ Laboratorium1/Controllers/HomeController.cs
@@ -27,6 +27,12 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult Calc(int a, int b)
+    {
+        int wynik = a + b;
+        ViewBag.Result = wynik;
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
